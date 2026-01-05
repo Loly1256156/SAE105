cbf1dfa6cbd9486af93f2dd423de2529bd8f3ab5
e2507466 <cr379122@gmail.com>
2026-01-05 14:42:57 +0100

    test

--- Sae105Csv.py
+++ Sae105Csv.py
@@ -1,3 +1,6 @@
+import csv
+
+
 etudiants = [
  {"ID": 1, "Nom": "Schmitt", "Prenom": "Albert", "Note": 9},
  {"ID": 2, "Nom": "Al-Hakim", "Prenom": "Yasmine", "Note": 17},
@@ -5,3 +8,9 @@ etudiants = [
  {"ID": 4, "Nom": "Meyer", "Prenom": "Claire", "Note": 16},
  {"ID": 5, "Nom": "Kobayashi", "Prenom": "Kaito", "Note": 11}
 ]
+
+with open('Sae105Csv.csv', mode='w', encoding='utf-8', newline='') as file:
+    champs = ["ID","Nom", "Prenom", "Note"]
+    fichier = csv.DictWriter(file, fieldnames=champs)
+    fichier.writeheader()
+    fichier.writerows(etudiants)
